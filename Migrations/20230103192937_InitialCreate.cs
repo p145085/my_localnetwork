1b43bdbee8534f20bb0d70842b3bf00e8df3916c
populus <emil.a.johansson@gmail.com>
2023-01-03 21:57:43 +0100

    Add project files.

---
+++ Migrations/20230103192937_InitialCreate.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace mylocalnetwork.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "WebPages",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IP = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Data = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    WhenSearched = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WebPages", x => x.ID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "WebPages");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using my_localnetwork;
+using System;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using System.Net.NetworkInformation;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+
+namespace my_localnetwork
+{
+    internal class Program
+    {
+        internal static async Task Initialize(WebScraperContext context)
+        {
+            context.Database.Migrate();
+        }
+
+        static void Main(string[] args)
+        {
+            // Configure the list of IP addresses to scrape
+            List<string> ips_list = new List<string>();
+            string baseip = "192.168.1.";
+            for (int i = 0; i < 256; i++)
+            {
+                ips_list.Add(baseip + i);
+            }
+            // Set the directory where the text files will be saved
+            string saveDirectory = "D:\\emiltempscraping";
+            // Use Entity Framework to connect to the database and retrieve the data
+            using (var db = new WebScraperContext())
+            {
+                db.Database.EnsureCreated();
+                foreach (string ip in ips_list)
+                {
+                    // Retrieve the web page data
+                    string data = GetWebPageData(ip);
+                    // Save the data to a text file
+                    if (data != null)
+                    {
+                        SaveToTextFile(data, saveDirectory, ip);
+                        // Use Entity Framework to add a record to the database
+                        db.WebPages.Add(new WebPage { IP = ip, Data = data });
+                    }
+                }
+                // Save the changes to the database
+                db.SaveChanges();
+            }
+            static string GetWebPageData(string ip)
+            {
+                // Use the WebClient class to retrieve the data
+                using (WebClient client = new WebClient())
+                {
+                    // Check if the IP address is pingable
+                    using (Ping ping = new Ping())
+                    {
+                        try
+                        {
+                            string url = "http://" + ip + ":80";
+                            PingReply reply = ping.Send(ip, 5000);
+                            if (reply.Status == IPStatus.Success)
+                            {
+                                // IP address is pingable, so download the data
+                                return client.DownloadString("http://" + ip + ":80");
+                            }
+                            else
+                            {
+                                // IP address is not pingable
+                                return null;
+                            }
+                        }
+                        catch (PingException ex)
+                        {
+                            return ex.Message;
+                        }
+                        catch (WebException wex) // 28
+                        {
+                            return wex.Message;
+                        }
+                    }
+                }
+            }
+            static void SaveToTextFile(string data, string saveDirectory, string ip)
+            {
+                // Create the file path
+                string filePath = Path.Combine(saveDirectory, ip + ".txt");
+
+                // Write the data to the file
+                File.WriteAllText(filePath, data);
+            }
+        }
+
+        
+
+        // Entity Framework database context
+        public class WebScraperContext : DbContext
+        {
+            public DbSet<WebPage> WebPages { get; set; }
+
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                // Configure the database connection string here
+                optionsBuilder.UseSqlServer(@"Server=(localdb)\\MSSQLLocalDB;Database=my_localnetwork;Trusted_Connection=True;");
+            }
+        }
+        // Entity Framework model for the WebPage table
+        public class WebPage
+        {
+            public int ID { get; set; }
+            public string? IP { get; set; }
+            public string? Data { get; set; }
+            public DateTime WhenSearched { get; set; } = DateTime.Now;
+        }
+    }
+}
